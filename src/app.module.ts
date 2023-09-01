171db0e84d33db1e9807cb106ce12dec2176c8f1
sunmin <sunmin0406@naver.com>
2023-09-01 14:40:34 +0900

    typeorm 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
 import { Boards2Module } from './boards2/boards2.module';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [BoardsModule, Boards2Module],
+  imports: [
+    BoardsModule,
+    Boards2Module,
+    TypeOrmModule.forRoot(typeORMConfig)
+  ],
 })
 export class AppModule {}
