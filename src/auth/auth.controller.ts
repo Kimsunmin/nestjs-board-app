6cb4e86994822b77efb3d048bfaab6aa33d221a8
sunmin <sunmin0406@naver.com>
2023-10-11 15:44:46 +0900

    로그인 기능 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -13,4 +13,9 @@ export class AuthController {
         return this.authService.signUp(authCredentialsDto);
     }
 
+    @Post('/signin')
+    signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<string>{
+        return this.authService.signIn(authCredentialsDto);
+    }
+
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,13 +2,19 @@ import { CustomRepository } from "src/typeorm/typeorm-ex.decorator";
 import { User } from "./user.entity";
 import { Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
-import { BadRequestException, ConflictException, InternalServerErrorException } from '@nestjs/common';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
-        const user = this.create({username, password});
+
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const user = this.create({username, password: hashedPassword});
+
         try{
             await this.save(user);
         } catch(error) {
