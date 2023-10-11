8409f448e9f83586ec54274b556c0f509079f9f0
sunmin <sunmin0406@naver.com>
2023-10-11 15:11:51 +0900

    불필요 제거

--- src/boards2/boards2.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { Boards2Controller } from './boards2.controller';
-import { Boards2Service } from './boards2.service';
-
-@Module({
-  controllers: [Boards2Controller],
-  providers: [Boards2Service]
-})
-export class Boards2Module {}
